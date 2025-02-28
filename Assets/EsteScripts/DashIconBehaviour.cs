a7d7deb7fdfbc33f719cd6876cbdde72e26bef84
Daron <Daronbaltazar@gmail.com>
2025-02-27 21:51:39 -0600

    dash etc.
    dash etc.

---
+++ Assets/EsteScripts/DashIconBehaviour.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro.Examples;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class DashIconBehaviour : MonoBehaviour
+{
+   // TextMeshProUGUI label;
+//   Image overlay;
+ //   float cooldown;
+//float cooldownRate;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       // label = GetComponentInChildren<TextMeshProUGUI>();
+       // Images[] images = GetComponentsInChildren<Image>();
+       // for (int i = 0; i < images.Length; i++)
+      //  {
+         //   if (images[i].tage == "overlay")
+        //    {
+       //         overlay = images[i];
+       //     }
+       // }
+     // cooldownRate = PinBehaviour.cooldownRate;
+     //   overlay.fillAmount = 0.0f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // cooldown = PinBehaviour.cooldown;
+       // string message = "";
+       // if (cooldown > 0.0)
+       // {
+       //     float fill = cooldown / cooldownRate;
+       //     message = string.Format("{0:0.0}", cooldown);
+            // message = cooldown.ToString();
+        //    overlay.fillAmount = fill;
+       // }
+       // label.SetText(message);
+        
+    }
+}
